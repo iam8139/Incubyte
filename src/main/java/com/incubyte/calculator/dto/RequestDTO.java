a89c6538896636291f1e993fa33b4a268346aa13
kash <iam8139@gmail.com>
2025-09-30 23:47:09 +0530

    Request, Response DTO defined and added positive test for controller

---
+++ src/main/java/com/incubyte/calculator/dto/RequestDTO.java
@@ -0,0 +1,12 @@
+package com.incubyte.calculator.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class RequestDTO {
+    private String input;
+}
